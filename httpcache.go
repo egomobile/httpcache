12d1611ab2aa031a8c9dc9e3735a668e279fa70c
Eric Kesseler <eric.kesseler@e-go-mobile.com>
2023-02-07 09:27:20 +0100

    first working version

--- httpcache.go
+++ httpcache.go
@@ -14,80 +14,44 @@ type Dataset struct {
 	Value string `json:"value"`
 }
 
-var dataSets = map[string]Dataset{}
+var DATASETS = map[string]Dataset{}
 
-func datasetsAsJson(key string) Dataset {
-	keys := make([]string, 0, len(dataSets))
-	values := make([]Dataset, 0, len(dataSets))
-
-	for k, v := range dataSets {
-		keys = append(keys, k)
-		values = append(values, v)
-	}
-
-	// jsonDatasets, err := json.Marshal(values)
-
+func getDataset(key string) Dataset {
 	var d Dataset
 
 	if key != "" {
-		value, ok := dataSets[key]
+		value, ok := DATASETS[key]
 		if ok {
 			d = value
 		} else {
-			fmt.Println("Value doesn't exist")
+			d.Key = ""
+			d.Value = ""
+			fmt.Println("Key doesn't exist")
 		}
 	}
 
 	return d
-
-	/* if err != nil {
-		fmt.Printf("Error: %s", err.Error())
-		return nil
-	} else {
-		return jsonDatasets
-	} */
 }
 
 func getDatasets(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	key := r.URL.Query().Get("key")
 	if key != "" {
-		value := datasetsAsJson(key)
-		// if value != nil {
-		b, err := json.Marshal(value)
-		if err != nil {
-			fmt.Println(err)
-			return
+		d := getDataset(key)
+		if d.Key != "" {
+			sendJsonResponse(w, 200, d)
+		} else {
+			sendResponse(w, 404, []byte("Key doesn't exist"))
 		}
-		fmt.Println(string(b))
-
-		sendResponse(w, 200, b)
-		// } else {
-		// sendResponse(w, 404, []byte("Value does not exist for this key!"))
-		//}
-	//} else {
-		// sendResponse(w, 200, []byte(datasetsAsJson("")))
-	//}
-}
-
-/* func getStringForDataset(key string, value []byte) string {
-	return "{\"key\": \"" + string(key) + "\", \"value\": \"" + string(value) + "\"}"
-}
-
-func getStringForDatasets() string {
-	dataSetsStr := "["
-	dataSetsLength := len(dataSets)
-	println(dataSetsLength)
-	i := 0
-	for key, value := range dataSets {
-		dataSetsStr += getStringForDataset(key, []byte(value))
-		if i < dataSetsLength-1 {
-			dataSetsStr += ","
+	} else {
+		var dataSets []Dataset
+		for key, value := range DATASETS {
+			print(key)
+			d := getDataset(value.Key)
+			dataSets = append(dataSets, d)
 		}
-		i++
+		sendJsonResponse(w, 200, dataSets)
 	}
-	dataSetsStr += "]"
-	return dataSetsStr
-}*/
+}
 
 func putDataset(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var d Dataset
@@ -98,9 +62,18 @@ func putDataset(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		return
 	}
 
-	dataSets[d.Key] = d
+	DATASETS[d.Key] = d
 
-	// sendResponse(w, 200, []byte(datasetsAsJson("")))
+	sendResponse(w, 404, []byte("Dataset sucessfully saved!"))
+}
+
+func sendJsonResponse(w http.ResponseWriter, status int, dataSets interface{}) {
+	j, err := json.Marshal(dataSets)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	sendResponse(w, status, j)
 }
 
 func sendResponse(w http.ResponseWriter, status int, data []byte) {
@@ -113,12 +86,12 @@ func Server() {
 	var d1 Dataset
 	d1.Key = "foo"
 	d1.Value = "bar"
-	dataSets[d1.Key] = d1
+	DATASETS[d1.Key] = d1
 
 	var d2 Dataset
 	d2.Key = "foo2"
 	d2.Value = "bar2"
-	dataSets[d2.Key] = d2
+	DATASETS[d2.Key] = d2
 
 	router := httprouter.New()
 	router.GET("/datasets", getDatasets)
